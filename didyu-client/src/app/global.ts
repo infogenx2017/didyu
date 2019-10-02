c392aec51f466391eb8827409ccb8d81c870fbda
Ujash Joshi <ujashj@whizlabs.com>
2019-10-02 19:32:53 +0530

    change API url

--- didyu-client/src/app/global.ts
+++ didyu-client/src/app/global.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class Globals {
     
-    // baseUrl: string = 'http://13.234.116.80:4206/';
-    baseUrl: string = 'http://didyu.local/';
+    baseUrl: string = 'http://13.234.116.80:4206/';
+    // baseUrl: string = 'http://didyu.local/';
 
 }
